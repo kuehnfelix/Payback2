2380e73eafb0279f5434854c268ee7e881a10e47
Felix Kühn <github@kuehnfelix.de>
2020-10-16 16:09:00 +0200

    Created paper project

---
+++ src/main/java/dekuehnfelix/payback2/Payback2.java
@@ -0,0 +1,12 @@
+package dekuehnfelix.payback2;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Payback2 extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+
+    }
+
+}
