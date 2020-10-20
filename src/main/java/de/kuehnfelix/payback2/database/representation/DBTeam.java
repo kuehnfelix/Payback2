f451888e9b1c7e675994e71f0578c6653fffc575
Felix Kühn <github@kuehnfelix.de>
2020-10-20 22:29:34 +0200

    Renamed id to name and added BsonProperty annotation

--- src/main/java/de/kuehnfelix/payback2/database/representation/DBTeam.java
+++ src/main/java/de/kuehnfelix/payback2/database/representation/DBTeam.java
@@ -1,33 +1,35 @@
 package de.kuehnfelix.payback2.database.representation;
 
 import de.kuehnfelix.payback2.config.Config;
+import org.bson.codecs.pojo.annotations.BsonProperty;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 public class DBTeam {
-    private String id;
+    @BsonProperty(value = "_id")
+    private String name;
     private List<DBPlayer> players;
     private int extraLives;
 
     public DBTeam(String name) {
-        this.id = name;
+        this.name = name;
         extraLives = Config.getConfig().getGameplay().getExtraTeamLives();
     }
 
     public DBTeam(String name, List<DBPlayer> players, int extraLives) {
-        this.id = name;
+        this.name = name;
         this.players = players;
         this.extraLives = extraLives;
     }
 
     public String getName() {
-        return id;
+        return name;
     }
 
     public void setName(String name) {
-        this.id = name;
+        this.name = name;
+        //TODO update Database
     }
 
     public List<DBPlayer> getPlayers() {
@@ -36,6 +38,7 @@ public class DBTeam {
 
     public void setPlayers(ArrayList<DBPlayer> players) {
         this.players = players;
+        //TODO update Database
     }
 
     public void addPlayer(DBPlayer p) {
@@ -51,12 +54,13 @@ public class DBTeam {
 
     public void setExtraLives(int extraLives) {
         this.extraLives = extraLives;
+        //TODO update Database
     }
 
     @Override
     public String toString() {
         return "Team{" +
-                "name='" + id + '\'' +
+                "name='" + name + '\'' +
                 ", players=" + players.toString() +
                 ", extraLives=" + extraLives +
                 '}';
