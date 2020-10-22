0e23ccaf903decda396009832de011b1067695ed
Felix Kühn <github@kuehnfelix.de>
2020-10-22 02:00:12 +0200

    Added PlayerCodec

--- src/main/java/de/kuehnfelix/payback2/database/codecs/PlayerCodec.java
+++ src/main/java/de/kuehnfelix/payback2/database/codecs/PlayerCodec.java
@@ -1,14 +1,30 @@
 package de.kuehnfelix.payback2.database.codecs;
 
 
+import de.kuehnfelix.payback2.database.representation.DBLocation;
 import de.kuehnfelix.payback2.database.representation.DBPlayer;
 import org.bson.BsonReader;
 import org.bson.BsonWriter;
+import org.bson.Document;
 import org.bson.codecs.Codec;
 import org.bson.codecs.DecoderContext;
+import org.bson.codecs.DocumentCodec;
 import org.bson.codecs.EncoderContext;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class PlayerCodec implements Codec<DBPlayer> {
+
+    private final Codec<Document> documentCodec;
+
+    public PlayerCodec() {
+        this.documentCodec = new DocumentCodec();
+    }
+
+    public PlayerCodec(final Codec<Document> codec) {
+        this.documentCodec = codec;
+    }
 
     @Override
     public DBPlayer decode(final BsonReader bsonReader, final DecoderContext decoderContext) {
@@ -17,8 +33,47 @@ public class PlayerCodec implements Codec<DBPlayer> {
     }
 
     @Override
-    public void encode(final BsonWriter bsonWriter, final DBPlayer dbPlayer, final EncoderContext encoderContext) {
-        //TODO implement encode
+    public void encode(final BsonWriter bsonWriter, final DBPlayer player, final EncoderContext encoderContext) {
+        final Document document = new Document();
+
+        final String name = player.getName();
+        final DBLocation location = player.getLocation();
+        final int folgen = player.getFolgen();
+        final boolean clearInv = player.isClearInv();
+        final boolean alive = player.isAlive();
+        final int strikes = player.getStrikes();
+        final String team = player.getTeam().getName();
+        final List<String> killed = player.getKilled().stream().map(p -> p.getName()).collect(Collectors.toList());
+
+        if(name != null) {
+            document.put("_id", name);
+        }
+
+        if(location != null) {
+            document.put("location", location);
+        }
+
+        if(folgen >= 0) {
+            document.put("folgen", folgen);
+        }
+
+        document.put("clearInv", clearInv);
+
+        document.put("alive", alive);
+
+        if(strikes >= 0) {
+            document.put("strikes", strikes);
+        }
+
+        if (team != null) {
+            document.put("team", team);
+        }
+
+        if (killed != null) {
+            document.put("killed", killed);
+        }
+
+        this.documentCodec.encode(bsonWriter, document, encoderContext);
     }
 
     @Override
